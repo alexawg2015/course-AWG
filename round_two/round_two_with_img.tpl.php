18e29a31ed136807e35a617f0eab58e5bfbf84fe
alexawg2015 <alex.awg.2015@gmail.com>
2015-06-10 09:23:27 +0300

    Round 2. Task 1.2.

---
+++ round_two/round_two_with_img.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @file
+* Template file for the theming example text form.
+*
+* Available custom variables:
+* - $img: HTML with image.
+* - $items: HTML with list, fieldset, table.
+*/
+?>
+
+<div class="image"><?php print $img; ?></div>
+<div class="items"><?php print $items; ?></div>
